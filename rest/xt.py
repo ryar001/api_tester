c2e56e97cf6c9d7db044964bfc8bd7780df107c9
jackson <jinshidiannao@jinshidiannaodeMacBook-Pro-2.local>
2025-07-25 11:44:55 +0800

    get_position should now get all if no symbol given

--- rest/xt.py
+++ rest/xt.py
@@ -87,7 +87,7 @@ class XtApi(RestBaseClass):
                 "symbol": symbol,
             }
 
-        header = api.um_perp_create_sign(api.__access_key, api.__secret_key, path=path, bodymod=bodymod,
+        header = api._create_sign(api.__access_key, api.__secret_key, path=path, bodymod=bodymod,
                                    params=params)
         header["Content-Type"] = "application/x-www-form-urlencoded"
         code, success, error = api._fetch(method="GET", url=url, headers=header, params=params, timeout=api.timeout)
